56a877e7084e1550c779ed2a9a9cf348b95de1b7
Halyna <gala.mrt@gmail.com>
2021-08-21 10:25:35 +0300

    v1.1 availablity improved

--- delivery_service.rb
+++ delivery_service.rb
@@ -1,5 +1,5 @@
 class DeliveryService
-  attr_accessor :cars_count, :bikes_count, :auto_park, :bike_park
+  attr_accessor :cars_count, :bikes_count, :auto_park
 
   require_relative 'car'
   require_relative 'bike'
@@ -7,23 +7,31 @@ class DeliveryService
 
   def initialize(cars_count = 6, bikes_count = 8)
     @auto_park = []
-    # @bike_park = []
     cars_count.times do
       @auto_park << Car.new
     end
     bikes_count.times do
       @auto_park << Bike.new
     end
+    @auto_park.sort_by! { |a| a.speed }
   end
 
   def find_transport
-    free_trans =  @auto_park.find { |a| a.is_bike? && a.available == true }
-    free_trans ||= @auto_park.find { |a| a.available == true }
-    output free_trans
+    free = false
+    free_trans = @auto_park.find { |a| a.available == true }
+    unless free_trans.nil?
+        free = true
+        free_trans.available = false
+    end
+    output free_trans, free
   end
 
-  def output(t)
-    p "#{t.is_a?(Car) ? 'Car' : 'Bike'} is ready for delivery. Delivery time is approx #{t.delivery_time} minutes"
+  def output(t, free)
+    if free
+      p "#{t.is_a?(Car) ? 'Car' : 'Bike'} is ready for delivery. Delivery time is approx #{t.delivery_time} minutes"
+    else
+      p 'No free transport'
+    end
   end
 
 end
